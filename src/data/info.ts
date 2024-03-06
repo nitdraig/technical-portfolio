70068afbccc48f5e9ba82b9032aeea74d739a66e
Agustin-desktop <agustin2051@gmail.com>
2024-03-05 22:45:41 -0300

    New

---
+++ src/data/info.ts
@@ -0,0 +1,208 @@
+export const info = {
+  experience: [
+    {
+      name: "Freelance (Remote)",
+      location: "Online bussiness",
+      startDate: "Dec 2023",
+      endDate: "Present",
+      description: [
+        "✓ I started my path in freelance jobs, to carry out new projects for other people, gain experience, learn new things and expand my skills.",
+        "✓ Technologies: NextJs, Tailwind, Github, ExpressJs, React native, Trello, Google drive.",
+      ],
+    },
+    {
+      name: "Fuddy (Remote)",
+      location: "Catamarca - Argentina",
+      startDate: "Sept 2023",
+      endDate: "Mar 2024",
+      description: [
+        "✓ I managed a team of 6 people, playing dual roles as Project Manager, Tech Lead, and Developer.",
+        "✓ I defined quality parameters to guarantee the delivery of the project.",
+        "✓ I established a cultural framework that encourages collaboration and innovation.",
+        "✓ I provided support in programming areas, guiding the team towards effective technical solutions.",
+        "✓ I directed the direction of the vision and mission of the project, aligning the team with the strategic objectives.",
+        "✓ Creator of project documentation and daily meeting guide to maintain team cohesion and effectiveness.",
+        "✓ I contributed to the development of Fuddy, an application with a purpose beyond the conventional, focused on creating environmental awareness.",
+        "✓ Technologies: NextJs, Tailwind, Github, ExpressJs, React native, Trello, Google drive.",
+      ],
+    },
+
+    {
+      name: "Advance Valley (Remote)",
+      location: "Catamarca - Argentina",
+      startDate: "Jan 2023",
+      endDate: "Jun 2023",
+      description: [
+        "✓ Collaborated in front-end development in several private projects, using React, MUI and JavaScript (JSX) to create intuitive and highly functional interfaces.",
+        "✓ I implemented attractive designs using HTML and CSS, applying the best web development practices. Collaborated closely with multidisciplinary teams to ensure consistency of design and functionality of the final product.",
+        "✓ Contributed to quality assurance by creating and maintaining detailed documentation, ensuring code integrity and development efficiency.",
+        "✓ Used agile methodologies to improve flexibility and speed in project delivery.",
+        "✓ Technologies: React.js, Github, Google Drive",
+      ],
+    },
+
+    {
+      name: "Tinta Negra Studios",
+      location: "Catamarca - Argentina",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "✓ Directed multimedia production and editing, providing creative and technical solutions for diverse projects.",
+        "✓ Contributed as a musician, applying Mastering & Mixing technical skills to optimize sound quality.",
+        "✓ I comprehensively managed the venture, which required interpersonal skills and a unique ability to address multiple responsibilities.",
+        "✓ Technologies: Fl Studio, Reaper, Ozone, Melodyne, Sony vegas, After Effects, Premiere, Da Vinci Resolve.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2019",
+      endDate: "2022",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
+    },
+    {
+      name: "Govt. Degree Boys College Buffer Zone",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["Intermediate, Pre-Engineering"],
+    },
+    {
+      name: "Ghazi Foundation Schools & Colleges",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Matriculation,  Computer Science"],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      projectName: "Skipy",
+      projectDescription:
+        "A multi-tool that seeks to centralize everyday resources for programmers and IT users. It has URL shortener, Password generator, QR code and Text encryptor.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1706016395/portolio-personal/m4lxn8etwagxrdxzxynu.png",
+      projectLinkGit: "https://skipy.top/ozxzrf",
+      projectLinkDeploy: "https://skipy.top/",
+      projectStack:
+        "Next.js, tailwind, Express, Cloudflare, Canny, Vercel, y GitHub.",
+    },
+    {
+      projectName: "Barber Template",
+      projectDescription:
+        "This template is a generic one that I created to offer freelance website services to small businesses and growing businesses, it is part of a project that I am developing in collaboration with startups. Developed in nextjs, using cloudinary to manage the images, tailwind for the styles and netlify for the display.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1703710922/plantillas/barberia/hndetznwqmznic3ydvxt.png",
+      projectLinkGit: "https://github.com/nitdraig/",
+      projectLinkDeploy: "https://templatebaber.netlify.app/",
+      projectStack: "Next.js, tailwind, y GitHub.",
+    },
+    {
+      projectName: "Ninja Challenge",
+      projectDescription:
+        "A project for Ninja Talent's React.js Jr. code challenge. An interface is developed that allows listing and viewing details of users individually, connected to 'back-end' that randomly brings up users and their information.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1703709297/portolio-personal/xncnb1pmzq77at8xdocn.png",
+      projectLinkGit: "https://github.com/nitdraig/ninja-challenge-fe.git",
+      projectLinkDeploy: "https://ninja-challenge-fe-ten.vercel.app",
+      projectStack: "React.js, Css, Javascript, y GitHub.",
+    },
+    {
+      projectName: "Personal Blog",
+      projectDescription:
+        "A personal blog to tell my experiences, some progress, opinions and various things related to the IT world and my passage through it, now available in Spanish and English. Built in Nextjs with tailwind.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1699054217/portolio-personal/hvc15pkogv8riltmvxy3.png",
+      projectLinkGit: "",
+      projectLinkDeploy: "https://en.blog.agustin.top",
+      projectStack: "Next.js, tailwind, GitHub & markdown",
+    },
+    {
+      projectName: "CFO-Bot",
+      projectDescription:
+        "A fullstack web built on nextjs on the front-end, tailwind for styles, express on the back-end and mongoDB to store chat interactions, makes use of the PaLM API for chat format queries. The API is set up to act as a CFO helping startups and entrepreneurs. Only respond to chats in English",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1694050182/portolio-personal/cltghxlaarfsj4yck4bv.webp",
+      projectLinkGit: "https://github.com/nitdraig",
+      projectLinkDeploy: "https://cfobot-fe.vercel.app",
+      projectStack:
+        "Next.js, Tailwind, Express.js, Node.js, MongoDB, GitHub & Yarn",
+    },
+    {
+      projectName: "Fuddy",
+      projectDescription:
+        "I collaborated in leading a team of 6 people, as project manager and developer. I defined quality parameters, establishment of a cultural framework, support in programming areas, direction of the project's vision and mission, creation of project documentation and daily meeting guide. In the development part I did front-end, back-end to a lesser extent, and most of mobile, with react native.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1702086792/fuddy/fpxgbfolktuj2le7u4sf.png",
+      projectLinkGit: "https://github.com/f-fuddy",
+      projectLinkDeploy: "https://fuddy.click",
+      projectStack:
+        "Next.js, Tailwind, Express.js, Node.js, MongoDB, React Native, GraphQL, GitHub & Yarn",
+    },
+    {
+      projectName: "Landing Page DRAIG",
+      projectDescription:
+        "A page made for my artistic side 'DRAIG', react, tailwind and various libraries were used in order to give it movement and responsiveness in various formats. Uploaded to vercel. The images are optimized and uploaded to the 'cloudinary' online file manager.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1689203365/portolio-personal/j1zztmcsj5mj3a7eagjs.png",
+      projectLinkGit: "https://github.com/nitdraig/draig-page",
+      projectLinkDeploy: "https://draig.art",
+      projectStack: "Next.js, Tailwind, Node.js, GitHub & Yarn",
+    },
+    {
+      projectName: "Various jobs in SmartHuerta",
+      projectDescription:
+        "I made various components for the 'Advance Valley' project called 'Smart Huerta', I can't attach the links but I leave some images to review.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1689207890/portolio-personal/wxy0cuonsonllclb277q.png",
+      projectLinkGit: "https://github.com/nitdraig",
+      projectLinkDeploy:
+        "https://drive.google.com/drive/folders/1_oambpe1_poCs0VMnTD4b9XyllufxZSL?usp=drive_link",
+      projectStack: "React.js, MUI, Node.js, & GitHub",
+    },
+    {
+      projectName: "Proyect in Alura bootcamp ",
+      projectDescription:
+        "A project made with React and different hooks from it, allows you to create new members in the organization, change color and assign roles to each one, as well as delete members with the click of a button. Uploaded to vercel.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1689203842/portolio-personal/v2nau1wgkx2d3ofeeq9d.png",
+      projectLinkGit: "https://github.com/nitdraig/org-proyect-alura",
+      projectLinkDeploy: "https://org-proyect-alura.vercel.app",
+      projectStack: "React.js, & GitHub",
+    },
+    {
+      projectName: "Landing page for TNS",
+      projectDescription:
+        "A landing page developed for 'Tinta negra studios' an independent music studio. It is developed in Next.js, using tailwind for styling, cloudinary to optimize image loading, and various animation libraries. Optimization for positioning was carried out through SEO. Vercel was used to deploy the project.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1689203304/portolio-personal/diuigsviz2nuybwzlewi.png",
+      projectLinkGit: "https://github.com/nitdraig/tinta-negra-studios-nextjs",
+      projectLinkDeploy: "https://tinta-negra-studios.vercel.app",
+      projectStack: "Next.js, Tailwind & GitHub,",
+    },
+    {
+      projectName: "Integrator Project",
+      projectDescription:
+        "Fullstack project for the course of Argentina Program. Front-End with Angular, bootstrap, CSS, and typescript. Uploaded to Firebase. Back-End with Spring, Java, MySQL and Json. Uploaded to Render. It has a login implemented, when logged in it allows you to modify specific sections and update it in the back-end.",
+      projectImg:
+        "https://res.cloudinary.com/draig/image/upload/v1689203290/portolio-personal/hzywdftudntjzxqnjowg.jpg",
+      projectLinkGit: "https://github.com/users/nitdraig/projects/2",
+      projectLinkDeploy: "https://a-agustin-portfolio-ap-68352.web.app",
+      projectStack:
+        "Java, SpringBoot, Angular, GitHub, Bootstrap, Typescript, Firebase, & MySQL",
+    },
+  ],
+};
